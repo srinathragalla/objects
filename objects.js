7029206eb86d3ac5f31e8dab75aa145cbaf855db
srinath ragalla <srinathragalla17@gmail.com>
2021-12-15 00:16:56 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+var tv = {
+    company : "sony", 
+    model:"bravia",
+    stock:150,
+};
+console.log(tv.model)
+console.log(tv)
+
+
+var tv2 = {};
+tv2.company="samsung";
+tv2.model="crystal";
+tv2.stock="120"
+console.log(tv2.company);
+console.log(tv2);
